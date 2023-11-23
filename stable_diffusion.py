27a8be6592f19d9609aef595bcf18aa8cfbc87ac
gbli <39871676+albblgb@users.noreply.github.com>
2023-11-23 11:01:45 +0800

    Update stable_diffusion.py

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -5,15 +5,15 @@ import random
 import os
 import config as c
 
-
 from utils.dir import mkdir
 
 os.environ["CUDA_VISIBLE_DEVICES"] = c.gpu_id
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# os.environ['CURL_CA_BUNDLE'] = ''
-
-# "key words: primeval forest, beach", "garden", "suburb, tower"
+'''
+     To run the code, you should download the pre-trained stable-diffusion-v1-5
+     we put the pre-trained model in '/root/.cache/huggingface/hub/models--runwayml--stable-diffusion-v1-5/snapshots/aa9ba505e1973ae5cd05f5aedd345178f52f8e6a'
+'''
 
 # 初始化，定义
 def init(device='cuda'):
@@ -23,8 +23,6 @@ def init(device='cuda'):
     ldm_stable.scheduler = DDIMScheduler.from_config(model_id, subfolder="scheduler")
     return ldm_stable
 
-
-
 def setup_seed(seed):
      # 设置随机数种子
      torch.manual_seed(seed)
@@ -33,19 +31,18 @@ def setup_seed(seed):
      random.seed(seed)
      torch.backends.cudnn.deterministic = True
 
-
 pipe = init()
 # pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
 pipe = pipe.to(device)
 
 
-prompt_list = ['']
-num_of_imgs_for_each_class = 2
+prompt_list = ['Campus']
+num_of_imgs_for_each_class = 3000
 img_save_dir = 'data/genearted_cover'
 
 
 for prompt in prompt_list:
-     img_dir = os.path.join(img_save_dir, prompt+'noprompt')
+     img_dir = os.path.join(img_save_dir, prompt)
      if not os.path.exists(img_dir):
           os.makedirs(img_dir)
 
